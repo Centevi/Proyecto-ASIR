fbe2beee9da2f0e74f0c5b60bff96f1a69a0dc51
sento <75783167+Centevi@users.noreply.github.com>
2024-04-09 18:07:29 +0200

    Update con.php

--- con.php
+++ con.php
@@ -1,5 +1,5 @@
 <?php
 //Inicio la sesi�n
 session_start();
-$con = mysqli_connect("localhost","galeria","qw16t@7-ZTvkfp5w","galeria") or die("No se pudo abrir la conexión: " . mysqli_connect_error());
+$con = mysqli_connect("localhost","","","") or die("No se pudo abrir la conexión: " . mysqli_connect_error());
 ?>
